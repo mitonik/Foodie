32f0a8b731e3045962a5da8a4cd061becd5bb7e9
Michał Antonik <xmitonikx@gmail.com>
2022-05-19 17:28:17 +0200

    Remove custom Add button

--- app/src/main/java/com/example/foodie/RecipeScreen.kt
+++ app/src/main/java/com/example/foodie/RecipeScreen.kt
@@ -6,29 +6,30 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.foodie.data.Recipes
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RecipeScreen(
-    navController: NavController,
-    list: List<Recipes>,
-    modifier: Modifier = Modifier
-) {
+fun RecipeScreen(navController: NavController, list: List<Recipes>) {
     Scaffold(floatingActionButton = {
-        AddButton { navController.navigate("details_screen") }
+        ExtendedFloatingActionButton(
+            text = { Text(stringResource(R.string.add)) },
+            icon = { Icon(Icons.Default.Add, contentDescription = null) },
+            onClick = {
+                navController.navigate(Screen.DetailsScreen.route)
+            }
+        )
     }) {
         Surface(modifier = Modifier.padding(it)) {
             LazyVerticalGrid(
-                modifier = modifier,
                 columns = GridCells.Fixed(2),
                 verticalArrangement = Arrangement.spacedBy(10.dp),
                 horizontalArrangement = Arrangement.spacedBy(10.dp),
@@ -40,9 +41,4 @@ fun RecipeScreen(
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun RecipeScreenPreview() {
 }
