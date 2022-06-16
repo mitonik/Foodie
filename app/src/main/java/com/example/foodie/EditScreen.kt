fe3ed15c6ec1adda322a26e0e2e0317cd5bba1b0
Michał Antonik <xmitonikx@gmail.com>
2022-06-16 22:02:35 +0200

    Prepare EditScreen

---
+++ app/src/main/java/com/example/foodie/EditScreen.kt
@@ -0,0 +1,41 @@
+package com.example.foodie
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.foodie.ui.theme.FoodieTheme
+
+@ExperimentalMaterial3Api
+@Composable
+fun EditScreen(
+    name: String,
+    description: String,
+) {
+    Scaffold {
+        Surface(Modifier.padding(it)) {
+            Column {
+                OutlinedTextField(
+                    value = name,
+                    onValueChange = {},
+                    label = { Text(stringResource(R.string.edit_recipe_name)) })
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = {},
+                    label = { Text(stringResource(R.string.edit_recipe_description)) })
+            }
+        }
+    }
+}
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun EditScreenPreview() {
+    FoodieTheme {
+        EditScreen("Name", "Desc")
+    }
+}
