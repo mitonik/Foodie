3ea3143447362773f00804ec905923862a461e9d
Michał Antonik <xmitonikx@gmail.com>
2022-06-17 19:19:12 +0200

    Update EditScreen design

--- app/src/main/java/com/example/foodie/ui/EditScreen.kt
+++ app/src/main/java/com/example/foodie/ui/EditScreen.kt
@@ -1,12 +1,16 @@
 package com.example.foodie.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.asLiveData
 import com.example.foodie.R
 import com.example.foodie.data.Graph
@@ -21,10 +25,16 @@ fun EditScreen(
     val recipe = Graph.recipeStore.loadRecipeById(id).asLiveData().observeAsState().value
     Scaffold {
         Surface(Modifier.padding(it)) {
-            Column {
+            Column(Modifier.padding(20.dp), verticalArrangement = Arrangement.spacedBy(20.dp)) {
                 if (recipe != null) {
+                    Image(
+                        painterResource(R.drawable.ic_launcher_background),
+                        contentDescription = null,
+                        Modifier.fillMaxWidth(),
+                        contentScale = ContentScale.Crop
+                    )
                     recipe.name?.let { it1 ->
-                        OutlinedTextField(
+                        TextField(
                             value = it1,
                             onValueChange = {
                                 runBlocking {
@@ -40,10 +50,11 @@ fun EditScreen(
                                     }
                                 }
                             },
+                            modifier = Modifier.fillMaxWidth(),
                             label = { Text(stringResource(R.string.edit_recipe_name)) })
                     }
                     recipe.description?.let { it1 ->
-                        OutlinedTextField(
+                        TextField(
                             value = it1,
                             onValueChange = {
                                 runBlocking {
@@ -54,7 +65,28 @@ fun EditScreen(
                                     }
                                 }
                             },
+                            modifier = Modifier.fillMaxWidth(),
                             label = { Text(stringResource(R.string.edit_recipe_description)) })
+                    }
+                    recipe.isFavourite?.let { it1 ->
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text("Favourite")
+                            Spacer(Modifier.weight(1f, true))
+                            Switch(checked = it1, onCheckedChange = { value ->
+                                runBlocking {
+                                    if (recipe.name != null) {
+                                        Graph.recipeStore.updateRecipes(
+                                            Recipe(
+                                                recipe.id,
+                                                recipe.name,
+                                                recipe.description,
+                                                value
+                                            )
+                                        )
+                                    }
+                                }
+                            })
+                        }
                     }
                 }
             }
