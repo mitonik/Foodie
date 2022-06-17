b2ba416ee532e508d9e7edfccf6e3c9be20619b8
Michał Antonik <xmitonikx@gmail.com>
2022-06-17 13:31:34 +0200

    Add Dark theme status to SettingsScreen

--- app/src/main/java/com/example/foodie/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/foodie/ui/SettingsScreen.kt
@@ -1,14 +1,39 @@
 package com.example.foodie.ui
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.foodie.R
 
+@ExperimentalMaterial3Api
 @Composable
 fun SettingsScreen() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Settings")
+    Scaffold(Modifier.fillMaxSize(), topBar = {
+        SmallTopAppBar(title = {
+            Text(stringResource(R.string.settings))
+        })
+    }) {
+        Surface(Modifier.padding(it)) {
+            Column {
+                Row(Modifier.padding(20.dp), verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = stringResource(R.string.dark_theme))
+                    Spacer(modifier = Modifier.weight(1f))
+                    Switch(checked = isSystemInDarkTheme(), onCheckedChange = {}, enabled = false)
+                }
+            }
+        }
     }
+}
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun SettingsScreenPreview() {
+    SettingsScreen()
 }
