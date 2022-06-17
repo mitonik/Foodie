c3c940d068939465d5b77fdab7f6f5ce7c339ed8
Michał Antonik <xmitonikx@gmail.com>
2022-06-17 11:23:43 +0200

    Fix recipe grid size

--- app/src/main/java/com/example/foodie/ui/RecipeScreen.kt
+++ app/src/main/java/com/example/foodie/ui/RecipeScreen.kt
@@ -2,10 +2,12 @@ package com.example.foodie.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
@@ -41,10 +43,13 @@ fun RecipeScreen(navController: NavController) {
     }) {
         Surface(modifier = Modifier.padding(it)) {
             LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                contentPadding = PaddingValues(10.dp)
+                GridCells.Fixed(2),
+                Modifier.fillMaxSize(),
+                rememberLazyGridState(),
+                PaddingValues(10.dp),
+                false,
+                Arrangement.spacedBy(10.dp),
+                Arrangement.spacedBy(10.dp)
             ) {
                 items(recipes) { recipe ->
                     RecipeCard(
