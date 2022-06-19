110cac88703de2c7afbe7f0dc617723edf51fb9d
Michał Antonik <xmitonikx@gmail.com>
2022-06-19 17:13:02 +0200

    Fix navigation bar colour

--- app/src/main/java/com/example/foodie/theme/Theme.kt
+++ app/src/main/java/com/example/foodie/theme/Theme.kt
@@ -5,16 +5,27 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun FoodieTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) darkColorScheme() else lightColorScheme()
+    val colors = if (darkTheme) {
+        darkColorScheme()
+
+    } else {
+        lightColorScheme()
+    }
 
     val systemUiController = rememberSystemUiController()
+    val color = if (darkTheme) Color(43, 40, 49) else Color(242, 237, 247)
     SideEffect {
-        systemUiController.setSystemBarsColor(
+        systemUiController.setStatusBarColor(
             color = colors.surface,
+            darkIcons = !darkTheme
+        )
+        systemUiController.setNavigationBarColor(
+            color = color,
             darkIcons = !darkTheme
         )
     }
