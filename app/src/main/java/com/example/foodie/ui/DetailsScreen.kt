5f683cc32c78d6cf801ffcc15a10e8e7cc8b34e5
Michał Antonik <xmitonikx@gmail.com>
2022-06-22 00:19:40 +0200

    Optimise DetailsScreen

--- app/src/main/java/com/example/foodie/ui/DetailsScreen.kt
+++ app/src/main/java/com/example/foodie/ui/DetailsScreen.kt
@@ -1,10 +1,11 @@
 package com.example.foodie.ui
 
-import android.graphics.Bitmap
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
@@ -14,27 +15,20 @@ import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.asLiveData
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.example.foodie.R
 import com.example.foodie.data.Graph
 import com.example.foodie.db.model.Recipe
 import kotlinx.coroutines.runBlocking
 
-
+@ExperimentalCoilApi
 @ExperimentalMaterial3Api
 @Composable
 fun DetailsScreen(
@@ -44,16 +38,14 @@ fun DetailsScreen(
 ) {
     val recipe = Graph.recipeStore.loadRecipeById(id).asLiveData().observeAsState().value
 
-    val context = LocalContext.current
-    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
-
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
             SmallTopAppBar(
                 title = {
                     if (recipe != null) {
-                        Text(recipe.name,
+                        Text(
+                            recipe.name,
                             overflow = TextOverflow.Ellipsis,
                             maxLines = 1
                         )
@@ -64,11 +56,19 @@ fun DetailsScreen(
                     IconButton(onClick = {
                         runBlocking {
                             if (recipe?.name != null && recipe.description != null
-                                && recipe.isFavourite != null) {
+                                && recipe.isFavourite != null
+                            ) {
                                 Graph.recipeStore.updateRecipes(
-                                    Recipe(recipe.recipeId, recipe.name,
-                                        recipe.proteins, recipe.fats, recipe.carbons, recipe.calories,
-                                        !recipe.isFavourite, recipe.description, recipe.imagePath
+                                    Recipe(
+                                        recipe.recipeId,
+                                        recipe.name,
+                                        recipe.proteins,
+                                        recipe.fats,
+                                        recipe.carbons,
+                                        recipe.calories,
+                                        !recipe.isFavourite,
+                                        recipe.description,
+                                        recipe.imagePath
                                     )
                                 )
                             }
@@ -88,11 +88,19 @@ fun DetailsScreen(
                     IconButton(onClick = {
                         runBlocking {
                             if (recipe?.name != null && recipe.description != null
-                                && recipe.isFavourite != null) {
+                                && recipe.isFavourite != null
+                            ) {
                                 Graph.recipeStore.deleteRecipe(
-                                    Recipe(recipe.recipeId, recipe.name,
-                                        recipe.proteins, recipe.fats, recipe.carbons, recipe.calories,
-                                        !recipe.isFavourite, recipe.description, recipe.imagePath
+                                    Recipe(
+                                        recipe.recipeId,
+                                        recipe.name,
+                                        recipe.proteins,
+                                        recipe.fats,
+                                        recipe.carbons,
+                                        recipe.calories,
+                                        !recipe.isFavourite,
+                                        recipe.description,
+                                        recipe.imagePath
                                     )
                                 )
                             }
@@ -106,184 +114,49 @@ fun DetailsScreen(
         }
     ) {
         Surface(Modifier.padding(it)) {
-            Column(modifier = Modifier
-                .verticalScroll(rememberScrollState())) {
-
-                val imageUri = rememberSaveable { mutableStateOf("") }
+            Column(Modifier.verticalScroll(rememberScrollState())) {
                 val painter = rememberImagePainter(
                     if (recipe?.imagePath == null) {
                         R.drawable.ic_launcher_background
-                    }
-                    else {
+                    } else {
                         recipe.imagePath
                     }
                 )
-
-                Row(horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp)) {
-                    Card(
-                        shape = RoundedCornerShape(3.dp),
-                        modifier = Modifier
-                            .size(380.dp)
-                    ) {
-                        Image(
-                            painter = painter,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            contentScale = ContentScale.Crop
-                        )
-                    }
-                }
-
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .padding(15.dp)
-                        .fillMaxWidth()) {
-
-                    recipe?.name?.let { it1 ->
-                        Text(
-                            it1,
-                            style = TextStyle(fontSize = 18.sp)
-                        )
-                    }
-                }
-
-                Row(horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(15.dp)) {
-
-                    //PROTEINS
-                    Text(
-                        stringResource(R.string.recipe_proteins),
-                        style = TextStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 18.sp
-                        ))
+                Image(
+                    painter,
+                    null,
+                    Modifier
+                        .size(500.dp)
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+                Column(Modifier.padding(10.dp)) {
                     if (recipe?.proteins != "") {
-                        recipe?.proteins?.let {
-                                it1 -> Text(
-                            it1,
-                            style = TextStyle(
-                                fontSize = 18.sp
-                            ))
+                        recipe?.proteins?.let { proteins ->
+                            Text("${stringResource(R.string.recipe_proteins)}: $proteins g")
                         }
                     }
-                    else {
-                        Text(" -",
-                            style = TextStyle(
-                                fontSize = 18.sp
-                            ))
-                    }
-                    Text(" [g]",
-                        style = TextStyle(
-                            fontSize = 18.sp
-                        ))
-                    Spacer(modifier = Modifier.width(10.dp))
-
-                    //FATS
-                    Text(stringResource(R.string.recipe_fats),
-                        style = TextStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 18.sp
-                        ))
                     if (recipe?.fats != "") {
-                        recipe?.fats?.let {
-                                it1 -> Text(
-                            it1,
-                            style = TextStyle(
-                                fontSize = 18.sp
-                            ))
+                        recipe?.fats?.let { fats ->
+                            Text("${stringResource(R.string.recipe_fats)}: $fats g")
                         }
                     }
-                    else {
-                        Text(" -",
-                            style = TextStyle(
-                                fontSize = 18.sp
-                            ))
-                    }
-                    Text(" [g]",
-                        style = TextStyle(
-                            fontSize = 18.sp
-                        ))
-                    Spacer(modifier = Modifier.width(10.dp))
-
-                    //CARBONS
-                    Text(stringResource(R.string.recipe_carbons),
-                        style = TextStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 18.sp
-                        ))
                     if (recipe?.carbons != "") {
-                        recipe?.carbons?.let {
-                                it1 -> Text(
-                            it1,
-                            style = TextStyle(
-                                fontSize = 18.sp
-                            ))
+                        recipe?.carbons?.let { carbons ->
+                            Text("${stringResource(R.string.recipe_carbons)}: $carbons g")
                         }
                     }
-                    else {
-                        Text(" -",
-                            style = TextStyle(
-                                fontSize = 18.sp
-                            ))
-                    }
-                    Text(" [g]",
-                        style = TextStyle(
-                            fontSize = 18.sp
-                        ))
-
-                }
-
-                Row(horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(2.dp)) {
-                    //CALORIES
-                    Text(stringResource(R.string.recipe_calories),
-                        style = TextStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp
-                        ))
                     if (recipe?.calories != "") {
-                        recipe?.calories?.let {
-                                it1 -> Text(
-                            it1,
-                            style = TextStyle(
-                                fontSize = 20.sp
-                            ))
+                        recipe?.calories?.let { calories ->
+                            Text("${stringResource(R.string.recipe_calories)}: $calories kcal")
                         }
                     }
-                    else {
-                        Text(" -",
-                            style = TextStyle(
-                                fontSize = 20.sp
-                            ))
-                    }
-                    Text(" [kcal]",
-                        style = TextStyle(
-                            fontSize = 18.sp
-                        ))
                 }
-
-                Column(modifier = Modifier.padding(15.dp)) {
-
-                    Text(stringResource(R.string.recipe_prepare),
-                        style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                recipe?.description?.let { description ->
+                    Text(
+                        description,
+                        Modifier.padding(10.dp)
                     )
-                    if (recipe != null) {
-                        recipe.description?.let { it1 ->
-                            Text(
-                                it1,
-                                style = TextStyle(fontSize = 18.sp)
-                            )
-                        }
-                    }
                 }
             }
         }
